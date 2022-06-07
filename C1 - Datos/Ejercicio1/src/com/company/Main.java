f80cee25b40eb07f9c17695fd0dfa9cfbe73ca15
camilabosio <cammibosio@gmail.com>
2022-06-07 17:54:00 -0300

    Clase 1 - Tipos de datos

---
+++ C1 - Datos/Ejercicio1/src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        String nombre = "Cami";
+        System.out.println("Hola " + nombre);
+    }
+}
---
+++ C1 - Datos/mesaDeTrabajo/src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String nombre;
+        int edad;
+        String alimento;
+        String sonido;
+
+
+        nombre = "Manchita";
+        edad = 2;
+        System.out.println(nombre + " tiene " + edad + " anios");
+        alimento = "1 kilo y medio" ;
+        sonido = "guau guau";
+        System.out.println(nombre + " come " + alimento + " y hace " + sonido);
+
+        nombre = "Nemo";
+        edad = 1 ;
+        System.out.println(nombre + " tiene " + edad + " anios");
+        alimento = "140 gramos";
+        sonido = "glu glu";
+        System.out.println(nombre + " come " + alimento + " y hace " + sonido);
+
+        nombre = "Silvestre";
+        edad = 3;
+        System.out.println(nombre + " tiene " + edad + " anios");
+        alimento = "medio kilo";
+        sonido = "Miauu";
+        System.out.println(nombre + " come " + alimento + " y hace " + sonido);
+
+        nombre = "Manuelita";
+        edad = 12;
+        System.out.println(nombre + " tiene " + edad + " anios");
+        alimento = "300 gramos";
+        sonido = "Peguajo";
+        System.out.println(nombre + " come " + alimento + " y hace " + sonido);
+
+        nombre = "Gardel";
+        edad = 1;
+        System.out.println(nombre + " tiene " + edad + " anios");
+        alimento = "350 gramos";
+        sonido = "pio pio";
+        System.out.println(nombre + " come " + alimento + " y hace " + sonido);
+
+
+
+
+
+    }
+}
